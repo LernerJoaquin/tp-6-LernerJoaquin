7fa2731df9b8755faf37f74bb1119305bc73b8a2
LernerJoaquin <lernerjoaquin@gmail.com>
2025-05-23 12:16:03 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,20 +1,61 @@
-# Replace the "ANSWER HERE" with your answer
-
 def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    list1 = list_to_remove_elements
+    if len(list1) < 2 :
+        return [ ]
+    elif len(list1) < 5 :
+        return list1[1:]
+    elif len(list1) == 5:
+        return list1[1 : -1]
+    elif len (list1) == 6:
+        return lsit1[1 : -2]
+    else:
+        list1 = list1[1:5] + list1[7:]
+        return list1
 
 def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    list1 = list_to_add_elements
+    list2 = ["Pink"]
+    list2[1:-1] = list1
+    list2 = list2[:]=["yellow"]
+
+    return list2
 
 
-def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
+    def is_empty(list_to_check):
+        list1 = list_to_check
+        list2 = [ ]
+        if list1 == list2:
+            return true
+        else:
+            return false
 
 
-def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
+def check_list(list_to_compare1, list_to_compare2):
+    list1 = list_to_compare1
+    list2 = list_to_compare2
+    if len (list1)> 2 and len (list2)>2:
+        if list[2] == list2[2]:
+            return true
+        else:
+            return false
+    else:
+        return false
 
+def lsit_of_lists(list_of_lists_to_modify):
+    list1 = list_of_lists_to_modify
+    if len(list1 [0]) < 3:
+        list1[0] = list1 [0] [0:]
+    else:
+        list1 [0] = list1 [0] [0:2]
+    if len(list1 [1]) < 2:
+        list1 [1] = []
+    elif len(list1 [1]) < 5:
+        list1 [1] = list1 [1] [1:]
+    else:
+        list1 [1] = list1 [1] [1:4]
+    if len(list1 [2]) < 3:
+        list1[2] = list1 [2]
+    else:
+        list[2] = list1[2] [-2:]
 
-def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+    return list1
